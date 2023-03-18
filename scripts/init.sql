a4dbec81abf6ec1ba2167f1fd37c27aefbf70822
Leopoldo Ramirez <info.ramirez.montes.leopoldo@gmail.com>
2023-03-17 18:54:58 -0600

    update settings for postgresql

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS tasks;
 
-CREATE TABLE tasks( 
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,     
+CREATE TABLE tasks (
+    id  SERIAL PRIMARY KEY,    
     task_text VARCHAR(200) NOT NULL,
     task_day VARCHAR(200) NOT NULL,
-    set_reminder TINYINT NOT NULL CHECK( set_reminder IN (0,1) ) DEFAULT 0
-); 
+    set_reminder BOOLEAN  DEFAULT false NOT NULL
+);
